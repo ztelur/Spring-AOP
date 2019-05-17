96a21d03156ca6b591fe802f307cddbc9b5b0258
Randy <ztelur@gmail.com>
2019-05-17 17:53:26 +0800

    update

--- src/main/java/com/remcarpediem/test/aop/service/OrderService.java
+++ src/main/java/com/remcarpediem/test/aop/service/OrderService.java
@@ -2,6 +2,7 @@ package com.remcarpediem.test.aop.service;
 
 import com.remcarpediem.test.aop.aop.advisor.Log;
 import com.remcarpediem.test.aop.aop.advisor.LogLevel;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
     @Log(value = LogLevel.ERROR, isTest = true)
     @Transactional
+    @Async
     public Long createOrder(Long userId, Long goodsId, Integer num) {
         return 1L;
     }
