1855255070ab16aeab274c4b84cb9cea1ace7845
Randy <ztelur@gmail.com>
2019-05-17 17:47:29 +0800

    update

--- src/main/java/com/remcarpediem/test/aop/AopApplication.java
+++ src/main/java/com/remcarpediem/test/aop/AopApplication.java
@@ -2,8 +2,10 @@ package com.remcarpediem.test.aop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class AopApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/remcarpediem/test/aop/service/OrderService.java
+++ src/main/java/com/remcarpediem/test/aop/service/OrderService.java
@@ -3,10 +3,12 @@ package com.remcarpediem.test.aop.service;
 import com.remcarpediem.test.aop.aop.advisor.Log;
 import com.remcarpediem.test.aop.aop.advisor.LogLevel;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
     @Log(value = LogLevel.ERROR, isTest = true)
+    @Transactional
     public Long createOrder(Long userId, Long goodsId, Integer num) {
         return 1L;
     }
